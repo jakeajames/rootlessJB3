a73c3a9aa7b55e59423f8df152d6e18d34b60769
Ignition Team <github@ignition.fun>
2019-02-06 07:59:00 +1100

    New UI!

---
+++ rootlessJB/UIDesignViewController.h
@@ -0,0 +1,17 @@
+//
+//  UIDesignViewController.h
+//  rootlessJB
+//
+//  Created by Joseph Shenton on 5/2/19.
+//  Copyright © 2019 Jake James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIDesignViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
